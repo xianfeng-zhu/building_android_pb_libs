f366ab80fa613db2688a0c8f98caf835e361d4d7
zhuxianfeng13 <zhuxianfeng13@gmail.com>
2019-12-07 20:50:07 +0800

    脚本整理

--- build_android.sh
+++ build_android.sh
@@ -17,44 +17,47 @@ fi
 
 ANDROID_SDK_CMAKE="$ANDROID_SDK/cmake/3.6.4111459/bin/cmake"
 ANDROID_NDK_TOOLCHAIN_CMAKE=$ANDROID_NDK/build/cmake/android.toolchain.cmake
-SCRIPT_DIR=$(pwd)
+SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 PB_VERSION="v3.7.0"
 rm -rf $SCRIPT_DIR/protobuf-$PB_VERSION
 git clone -b $PB_VERSION --recurse-submodules -j8 https://github.com/protocolbuffers/protobuf.git $SCRIPT_DIR/protobuf-$PB_VERSION
 
 if [[ $? -ne 0 ]]; then
-    echo "failed 2222222222"
+    echo "git clone failed"
     exit 1
 fi
+
+ANDROID_ABI=arm64-v8a
+ANDROID_API_LEVEL=16
 
 rm -rf build
 mkdir build
 cd build
 
 $ANDROID_SDK_CMAKE \
-    -Dprotobuf_BUILD_SHARED_LIBS=true \
+    -Dprotobuf_BUILD_SHARED_LIBS=false \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_TOOLCHAIN_CMAKE \
     -DCMAKE_INSTALL_PREFIX=./protobuf \
     -DANDROID_NDK=$ANDROID_NDK \
     -DANDROID_TOOLCHAIN=clang \
-    -DANDROID_ABI=arm64-v8a \
-    -DANDROID_NATIVE_API_LEVEL=16 \
+    -DANDROID_ABI=$ANDROID_ABI \
+    -DANDROID_NATIVE_API_LEVEL=$ANDROID_API_LEVEL \
     -DANDROID_STL=c++_shared \
     -DANDROID_LINKER_FLAGS="-landroid -llog" \
     -DANDROID_CPP_FEATURES="rtti exceptions" \
     $SCRIPT_DIR/protobuf-$PB_VERSION/cmake
 
 if [[ $? -ne 0 ]]; then
-    echo "failed 2222222222"
+    echo "android_cmake config failed"
     exit 1
 fi
 
 $ANDROID_SDK_CMAKE --build .
 
 if [[ $? -ne 0 ]]; then
-    echo "failed 33333333333"
+    echo "build failed"
     exit 1
 fi
 
